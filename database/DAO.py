4d38473b4a4768b98497605569a06f986536cec7
iredlp <s311559@studenti.polito.it>
2026-08-30 16:30:57 +0200

    rifaccio l'es

--- database/DAO.py
+++ database/DAO.py
@@ -21,3 +21,29 @@ class DAO():
         cursor.close()
         conn.close()
         return result
+
+    @staticmethod
+    def getAllNodes():
+        conn = DBConnect.get_connection()
+
+        result = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = """ select t.ID, t.IATA_CODE, count(*) as N
+                    from(select a.ID, a.IATA_CODE, f.AIRLINE_ID 
+                    from airports a, flights f 
+                    where a.ID =f.ORIGIN_AIRPORT_ID or a.ID=f.DESTINATION_AIRPORT_ID 
+                    group by a.ID, a.IATA_CODE, f.AIRLINE_ID
+                    ) t
+                    group by t.ID, t.IATA_CODE
+                    having N>=5
+                    order by N asc"""
+
+        cursor.execute(query)
+
+        for row in cursor:
+            result.append(Airport(**row))
+
+        cursor.close()
+        conn.close()
+        return result
